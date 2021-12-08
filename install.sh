e292bc10421a8db30559ab1d5349724536824890
WaXxX <59426207+Waxxx333@users.noreply.github.com>
2021-12-08 01:00:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ DARK=('\033[38;5;245m');
 BLUE=('\033[38;5;50m');
 RED=('\033[38;5;9m');
 BOLD=('\033[01m');
+Version=('v2');
 if [[ ${UID} != 0 ]]; then
     if [[ -d /data/data/com.termux ]]; then
         echo -e "Termux Detected"
@@ -15,21 +16,21 @@ if [[ ${UID} != 0 ]]; then
     fi
 fi
 if [[ -f ./py2marshal.py ]]; then
-    if [[ -d /data/data/com.termux && ${UID} != 0 ]]; then
-        echo -e "${GREEN}Termux${WHITE}: ${BLUE}Installing ${PURPLE}py2marshal.py ${BLUE}to ${GREEN}/bin/${PURPLE}py2marshal"
-        echo -e "${BLUE}To run it${WHITE}, ${BLUE}just type ${WHITE}'${GREEN}py2marshal${WHITE}'"
-        cp py2marshal.py /data/data/com.termux/files/usr/bin/py2marshal
-    elif [[ -d /bin && ${UID} = 0 ]]; then
+    if [[ -d /bin && ${UID} = 0 ]]; then
         echo -e "${GREEN}Linux${WHITE}: ${BLUE}Installing ${PURPLE}py2marshal.py ${BLUE}to ${GREEN}/bin/${PURPLE}py2marshal"
         echo -e "${BLUE}To run it${WHITE}, ${BLUE}just type ${WHITE}'${GREEN}py2marshal${WHITE}'"
         cp py2marshal.py /bin/py2marshal
+    elif [[ -d /data/data/com.termux && ${UID} != 0 ]]; then
+        echo -e "${GREEN}Termux${WHITE}: ${BLUE}Installing ${PURPLE}py2marshal.py ${BLUE}to ${GREEN}/bin/${PURPLE}py2marshal"
+        echo -e "${BLUE}To run it${WHITE}, ${BLUE}just type ${WHITE}'${GREEN}py2marshal${WHITE}'"
+        cp py2marshal.py /data/data/com.termux/files/usr/bin/py2marshal
     else
         echo -e ${RED}"No /bin. Not using Linux ? Come back later for a better installer that'll work with your operating system."
     fi
 fi
-if [[ -d /usr/share/bash-completion ]]; then 
+if [[ -d /usr/share/bash-completion && ${UID} = 0 ]]; then 
     cp py2marshal.completion /usr/share/bash-completion/completions/py2marshal
-elif [[ -d /data/data/com.termux/files/usr/share/bash-completion ]]; then 
+elif [[ -d /data/data/com.termux/files/usr/share/bash-completion && ${UID} != 0 ]]; then 
     cp py2marshal.completion /data/data/com.termux/files/usr/share/bash-completion/completions/py2marshal
 else
     echo -e ${RED}"No bash completion installed. Cannot install the tab completion script right now."
